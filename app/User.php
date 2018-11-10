e364201a447467468d6793cab64d40cbdeae245a
Pablo Bonillo Molina <pbonillo96@gmail.com>
2018-11-10 19:36:50 +0100

    modelos

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nombre','apellidos','descripcion_user', 'email', 'password',
     ];
 
     /**
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function post()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
