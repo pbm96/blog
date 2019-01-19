b0b0784ade579849bbc7f57ed52ae6e76d06a219
Pablo Bonillo Molina <pbonillo96@gmail.com>
2019-01-19 20:18:39 +0100

    google adsense

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -30,6 +30,15 @@
         gtag('config', 'UA-132770331-1');
     </script>
 
+    <!--Google adsense-->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-7435489657420119",
+            enable_page_level_ads: true
+        });
+    </script>
+
         @yield('estilos')
 
 
